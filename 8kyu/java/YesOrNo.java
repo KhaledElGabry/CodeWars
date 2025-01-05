77e4b887ad43ca0cc5a247279c9c646357ce2858
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-01-05 12:19:51 +0200

    Modify my solution for Yes or No problem, make it more readable

--- 8kyu/java/YesOrNo.java
+++ 8kyu/java/YesOrNo.java
@@ -1,9 +1,10 @@
 class YesOrNo {
     // simple if condition
     public static String ifBoolToWord(boolean b) {
-        if (!b) return "No";
-        return "Yes";
+        if (b) return "Yes";
+        return "No";
     }
+
     // ternary operator
     public static String boolToWord(boolean b) {
         return b ? "Yes" : "No";
